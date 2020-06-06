7efc15a0266459a4ad83d0209c42834b06e894fe
Erick Leyva Díaz <userley.diaz@gmail.com>
2020-06-05 23:40:18 -0500

    Archivos Actualizados css y js

--- head.php
+++ head.php
@@ -24,6 +24,7 @@ switch ($link) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
@@ -35,9 +36,9 @@ switch ($link) {
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/bootstrap-select/css/bootstrap-select.min.css">
     <script src="assets/js/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/bootstrap-select/js/bootstrap-select.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
     <script src="assets/js/freelancer.js"></script>
 </head>
 
